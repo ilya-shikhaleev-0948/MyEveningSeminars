4870ccfb2fad39e3133738a5a3adc4be749b6458
il <ilya_shikhaleev@mail.ru>
2022-12-21 14:29:07 +0300

    Сделал домашнее задание

--- Homeworks/Homeworks1/Program.cs
+++ Homeworks/Homeworks1/Program.cs
@@ -4,20 +4,131 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
-Console.WriteLine("Input first number");
-int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Input second number");
-int b = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input first number");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input second number");
+// int b = Convert.ToInt32(Console.ReadLine());
 
-int max = a < b; 
+// int max = a; 
 
-if (max == a)
-{
-    Console.WriteLine($"{a} is max of {b}");
-}
-else
- {
-    Console.WriteLine($"{a} is not max of {b}");
- }
+// if (max == a)
+// {
+//     Console.WriteLine($"{a} < {b}");
+// }
+// else
+//  {
+//     Console.WriteLine($"{a} > {b}");
+//  }
+
+
+// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+// Console.WriteLine("Input first number");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input second number");
+// int b = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input three number");
+// int c = Convert.ToInt32(Console.ReadLine());
+
+// int max = a;
+
+// if (b > max) max=b;
+// if (c > max) max=c;
+// {
+// Console.WriteLine($"max{max}");   
+// }
+
+//  Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+// Console.WriteLine(" Input Number ");
+// int x =Convert.ToInt32(Console.ReadLine());
+// int parity = x / 2;
+// if (x % 2==0)
+// {
+//    Console.WriteLine($"{x} yes parity ");  
+// }
+// else
+// {
+//     Console.WriteLine($"{x} not parity ");
+// }
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+// Console.WriteLine("Input number N");
+// int N = Convert.ToInt32(Console.ReadLine());
+// int minnamber = 1;
+
+// while (minnamber <= N)
+// {
+
+//     if (N % 2 == 0)
+//     {
+//         Console.WriteLine($"{N}");
+//     }
+
+// minnamber++;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 

df61c9fc8996339c92ecc5320f8f5f105113e8bf
il <ilya_shikhaleev@mail.ru>
2022-12-25 20:26:00 +0300

    Добавил домашнюю работу №4

---
+++ Homeworks/Homrworks4/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и
+// возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+double FindDegree( double a,int  b)
+{
+    if (b < 0) {(a, b) = (1.0 / a, -b);}
+    {
+
+        double degree = 1.0;
+        for (; b != 0; a *= a, b /= 2)
+        {
+            if (b % 2 != 0) { degree *= a; }
+        }
+        return degree;
+    }
+}
+Console.WriteLine("Input A:");
+double a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input B:");
+int b = Convert.ToInt32(Console.ReadLine());
+double result = FindDegree(a,b);
+Console.WriteLine(result);
+
+
+// Задача 27: Напишите программу, которая принимает на вход число и 
+// выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+// int getSum(int n)
+// {
+//     int sum = 0;
+//     while (n != 0)
+//     {
+//         sum = sum + n % 10;
+//         n = n / 10;
+//     }
+//     return sum;
+// }
+
+// Console.WriteLine("Введите число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int result = getSum(n); 
+// Console.WriteLine(result);
---
+++ Seminars/Seminars4/Program.cs
@@ -0,0 +1,108 @@
+﻿// Задача 1. Напишите программу, которая принимает на вход число (А) 
+// и выдаёт сумму чисел от 1 до А.
+
+// int FindSum (int num)
+// {
+// int sum = 0;
+// for(int current= 1; current<=num;current++)
+// {
+// sum += current;
+// }
+// return sum;
+// }
+
+// Console.WriteLine("Input A:");
+// int a = Convert.ToInt32(Console.ReadLine());
+// int result = FindSum(a);
+// Console.WriteLine(result);
+
+
+//Задача 2.Напишите программу, которая принимает на вход число и
+// выдаёт количество цифр в числе.
+
+//456 -> 3
+//78 -> 2
+//89126 -> 5
+
+//Задача 2.Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+//456 -> 3
+//78 -> 2
+//89126 -> 5
+
+// int Number (int num)
+// {
+// int count = 0;
+// while (num != 0)
+// {
+// num /= 10;
+// count++;
+// }
+// return count;
+// }
+
+// Console.WriteLine("Input :");
+// int a = Convert.ToInt32(Console.ReadLine());
+// int result = Number(a);
+// Console.WriteLine(result);
+
+
+//Задача 3. Напишите программу, которая принимает на вход число N 
+//и выдаёт произведение чисел от 1 до N.
+
+//4 -> 24
+//5 -> 120
+
+
+// int Factorial (int num)
+// {
+// int fact = 1;
+// for(int current = 1; current<=num;current++)
+// {
+// fact = fact * current;
+// }
+// return fact;
+// }
+
+// Console.WriteLine("Input : ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// int result = Factorial(a);
+// Console.WriteLine(result);
+
+
+//Напишите программу, которая выводит массив из 8 элементов,
+//заполненный нулями и единицами в случайном порядке.
+// [1,0,1,1,0,1,0,0]
+
+
+// int[] CreateRandomArray (int size,int minValue, int maxValue)
+// {
+// int[] myArray = new int[size];
+// for(int i=0;i<size;i++)
+// {
+// myArray[i] = new Random().Next(minValue,maxValue+1);
+// }
+// return myArray;
+// }
+
+// void ShowArray (int[] array)
+// {
+// Console.WriteLine("Полученный массив ->:");
+// for(int i = 0; i < array.Length; i++)
+// {
+// Console.Write(array[i]+" ");
+// }
+// Console.WriteLine();
+// }
+// Console.WriteLine("Введите размер массива:");
+// int lenght = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите минимально возможное значение элемента массива:");
+// int min = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите максимально возможное значение элемента массива:");
+// int max = Convert.ToInt32(Console.ReadLine());
+
+// // int[] newArray = CreateRandomArray(lenght,min,max);
+// // ShowArray(newArray);
+
+// ShowArray(CreateRandomArray(lenght,min,max));
+
+

ca737ea4c4c7113549df45b61760d2ce61c58285
il <ilya_shikhaleev@mail.ru>
2022-12-27 16:45:04 +0300

    добавил Домашнюю работу 5

---
+++ Homeworks/Homeworks5/Program.cs
@@ -0,0 +1,153 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными
+// трёхзначными числами. Напишите программу, которая покажет количество
+// чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+// int[] CreateRandomArray(int size, int minValue, int maxValue)
+// {
+
+//     int[] result = new int[size];
+//     for (int i = 0; i < result.Length; i++)
+//     {
+//         result[i] = new Random().Next(minValue, maxValue + 1);
+//     }
+//     return result;
+// }
+
+// void ShowArray (int[] array)
+// {
+// Console.WriteLine("Полученный массив ->:");
+// for(int i = 0; i < array.Length; i++)
+// {
+// Console.Write(array[i]+" ");
+// }
+// Console.WriteLine();
+// }
+
+// void EvenNumbers(int[] MyArray)
+// {
+
+//     int sun = 0;
+//     for (int i = 0; i < MyArray.Length; i++)
+//     {
+//         if (MyArray[i]%2==0) 
+//         {
+//             sun = sun + 1;
+//         }
+//     }
+//     Console.WriteLine($"Чётных чисел {sun}");
+// }
+
+// Console.WriteLine("Введите размер массива:");
+// int lenght = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите минимально возможное значение элемента массива=100");
+// int min = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите максимально возможное значение элемента массива=999");
+// int max = Convert.ToInt32(Console.ReadLine());
+
+// int[] newArray = CreateRandomArray(lenght,min,max);
+
+// ShowArray(newArray);
+// EvenNumbers(newArray);
+
+
+// Задача 36: Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+// int[] CreateRandomArray (int size,int minValue, int maxValue)
+// {
+// int[] myArray = new int[size];
+// for(int i=0;i<size;i++)
+// {
+// myArray[i] = new Random().Next(minValue,maxValue+1);
+// }
+// return myArray;
+// }
+
+// void ShowArray (int[] array)
+// {
+// Console.WriteLine("Полученный массив ->:");
+// for(int i = 0; i < array.Length; i++)
+// {
+// Console.Write(array[i]+" ");
+// }
+// Console.WriteLine();
+// }
+
+// void FindMinSum (int[] array)
+// {
+//     int sumNegative = 0;
+//     for(int i = 0; i < array.Length;i++)
+//     {
+//         if ((i++ % 2) == 0)
+//         {
+//             if(array[i] > 0) sumNegative += array[i];
+//         }
+//     }
+//     Console.WriteLine($"Сумма элементов на нечётных позициях {sumNegative}");
+// }
+
+// Console.WriteLine("Введите размер массива:");
+// int lenght = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите минимально возможное значение элемента массива:");
+// int min = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите максимально возможное значение элемента массива:");
+// int max = Convert.ToInt32(Console.ReadLine());
+
+// int[] newArray = CreateRandomArray(lenght,min,max);
+
+// ShowArray(newArray);
+// FindMinSum(newArray);
+
+
+
+// Задача 38: Задайте массив вещественных чисел.
+// Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+// int size = 5;
+// int[] numbers = new int[size];
+// FillArrayRandomNumbers(numbers);
+// PrintArray(numbers);
+
+// int max = numbers[0];
+// int min = numbers[0];
+
+// for (int i = 0; i < numbers.Length; i++)
+// {
+//     if (numbers[i] > max)
+//     {
+//         max = numbers[i];
+//     }
+//     else if (numbers[i] < min)
+//     {
+//         min = numbers[i];
+//     }
+// }
+
+// Console.WriteLine($"Минимальное число: {min}");
+// Console.WriteLine($"Максимальное число: {max}");
+// Console.WriteLine($"Разница между максимальным и минимальным числами: {max-min}");
+
+// void FillArrayRandomNumbers(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = new Random().Next(1, 100);
+//     }
+// }
+
+// void PrintArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+//     Console.WriteLine();
+// }
---
+++ Seminars/Seminars6/Program.cs
@@ -0,0 +1,147 @@
+﻿//Задача 1. Напишите программу, которая перевернёт одномерный массив
+// (последний элемент будет на первом месте,
+//а первый - на последнем и т.д.)
+
+//[1 2 3 4 5] -> [5 4 3 2 1]
+//[6 7 3 6] -> [6 3 7 6]
+
+
+// int[] CreateRandomArray(int size, int minValue, int maxValue)
+// {
+// int[] myArray = new int[size];
+// for (int i = 0; i < size; i++)
+// {
+// myArray[i] = new Random().Next(minValue, maxValue + 1);
+// }
+// return myArray;
+// }
+
+// void ShowArray(int[] array)
+// {
+// Console.WriteLine("Полученный массив ->:");
+// for (int i = 0; i < array.Length; i++)
+// {
+// Console.Write(array[i] + " ");
+// }
+// Console.WriteLine();
+// }
+
+// int[] ReverseArray(int[] array)
+// {
+// for(int i = 0; i < array.Length/2; i++) // 4 6 8 1
+// {
+// int temp = array[i];
+// array[i] = array[array.Length - 1 - i];
+// array[array.Length - 1 - i] = temp;
+// }
+// return array;
+// }
+
+
+// Console.WriteLine("Введите размер массива:");
+// int lenght = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите минимально возможное значение элемента массива:");
+// int min = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите максимально возможное значение элемента массива:");
+// int max = Convert.ToInt32(Console.ReadLine());
+
+// int[] newArray = CreateRandomArray(lenght,min,max);
+
+// ShowArray(newArray);
+// newArray = ReverseArray(newArray);
+// ShowArray(newArray);
+
+
+// Задача 2.Напишите программу, которая принимает на вход три числа и
+// проверяет, может ли существовать треугольник с сторонами такой длины.
+// самое важное знать правило, какждая сторона должна быть 
+// меньше(строго) суммы двух других сторон
+
+// bool CheckTriangle (int x, int y, int z)
+// {
+// if((x<y+z) && (y<x+z) && (z<x+y))
+// {
+// return true;
+// }
+// return false;
+// }
+
+// Console.WriteLine("Введите длину стороны a");
+// int x = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите длину стороны b");
+// int y = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите длину стороны c");
+// int z = Convert.ToInt32(Console.ReadLine());
+
+// //Console.WriteLine($"CheckTriangle(x,y,z)");
+
+// bool res = CheckTriangle(x,y,z);
+// if(res == true)
+// {
+// Console.WriteLine($"Треугольник со сторонами {x} {y} {z} может существовать ");
+// }
+// else
+// Console.WriteLine($"Треугольник со сторонами {x} {y} {z} не может существовать ");
+
+
+//Задача 3. Не используя рекурсию, выведите первые N чисел Фибоначчи.
+// Первые два числа Фибоначчи: a и b.
+
+//Числа фибоначчи последовательность чисел в которой
+// первые 2 числа это 0 и 1, а последубщие это сумма двух передыдущих
+//[0, 1,2, 3, 5, 8, 13, ... ]
+
+// int[] CreateRandomArray(int n, int a, int b)
+// {
+// int[] myArray = new int[n];
+// myArray[0] = a;
+// myArray[1] = b;
+// for (int i = 2; i < n; i++)
+// {
+// myArray[i] = myArray[i-2] + myArray[i-1];
+// }
+// return myArray;
+// }
+
+// void ShowArray(int[] array)
+// {
+// Console.WriteLine("Полученный массив ->:");
+// for (int i = 0; i < array.Length; i++)
+// {
+// Console.Write(array[i] + " ");
+// }
+// Console.WriteLine();
+// }
+
+
+// Console.WriteLine("Введите размер массива:");
+// int lenght = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите первый элемент массива:");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второй элемент массива :");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// int[] newArray = CreateRandomArray(lenght,a,b);
+
+// ShowArray(newArray);
+
+
+//Задача 4. ****Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+
+//45 -> 101101
+//3 -> 11
+//2 -> 10
+
+// string Method4(int num)
+// {
+// string result = string.Empty;
+// while (num > 0)
+// {
+// result = num%2 +result;
+// num = num / 2;
+// }
+// return result;
+// }
+// string res = Method4(2);
+// Console.WriteLine(res);
